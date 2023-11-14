3c3f3d67d83ffaf7421368c79a72c2c887f248b1
Yisen <yisenliu@gmail.com>
2023-11-14 08:48:57 +0800

    update timeout to 8s

--- src/markets/tw/hooks/useTaiwanStockNews.js
+++ src/markets/tw/hooks/useTaiwanStockNews.js
@@ -17,7 +17,7 @@ export default function useTaiwanStockNews(ticker, token = null) {
       url: process.env.GithubPages
         ? corsProxy + encodeURIComponent('https://api.finmindtrade.com/api/v4/data' + paramsStr)
         : '/api/stock' + paramsStr,
-      timeout: 5000,
+      timeout: 8000,
       headers: { 'content-type': 'application/x-www-form-urlencoded', accept: 'application / json' },
     },
     [ticker],
